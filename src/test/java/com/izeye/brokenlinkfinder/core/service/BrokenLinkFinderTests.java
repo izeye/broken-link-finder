031e8c546d45fecb6a49330b4d95d21f00a5143f
Johnny Lim <izeye@naver.com>
2015-12-18 21:00:53 +0900

    Ignore known invalid URLs in Spring Boot reference documentation

--- src/test/java/com/izeye/brokenlinkfinder/core/service/BrokenLinkFinderTests.java
+++ src/test/java/com/izeye/brokenlinkfinder/core/service/BrokenLinkFinderTests.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by izeye on 15. 10. 22..
  */
@@ -14,14 +18,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(Application.class)
 public class BrokenLinkFinderTests {
 	
+	private static final Set<String> SPRING_BOOT_KNOWN_INVALID_URLS;
+	
+	static {
+		Set<String> springBootKnownInvalidUrls = new HashSet<>();
+		springBootKnownInvalidUrls.add("https://uaa.run.pivotal.io/userinfo");
+		springBootKnownInvalidUrls.add("https://uaa.run.pivotal.io/check_token");
+		springBootKnownInvalidUrls.add("http://acloudyspringtime.cfapps.io/");
+		SPRING_BOOT_KNOWN_INVALID_URLS = Collections.unmodifiableSet(springBootKnownInvalidUrls);
+	}
+	
 	@Autowired
 	BrokenLinkFinder brokenLinkFinder;
 	
 	@Test
 	public void test() {
 		String url = "http://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/";
+//		String url = "http://docs.spring.io/spring-restdocs/docs/current-SNAPSHOT/reference/html5/";
 		brokenLinkFinder.find(url).stream()
-				.filter(link -> !link.getUrl().contains("://localhost"))
+				.filter(link -> !link.getUrl().contains("://localhost")
+						&& !SPRING_BOOT_KNOWN_INVALID_URLS.contains(link.getUrl()))
 				.forEach(System.out::println);
 	}
 	
